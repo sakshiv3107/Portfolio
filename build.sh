1d5df15d89db603ae1ed733cb6ccdaef8bc4fbeb
Sakshi Vishnoi <sakshi.vishnoi3107@gmail.com>
2026-06-09 02:25:44 +0530

    fix build - local build passing

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-# Install Flutter
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
 
-# Verify flutter works
 flutter --version
-
-# Get dependencies and build
 flutter pub get
-flutter build web --release
+flutter build web --release --no-tree-shake-icons
