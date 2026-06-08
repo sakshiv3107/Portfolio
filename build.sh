5eb20dc63b5463282ab14a6e091079cad5f368e6
Sakshi Vishnoi <sakshi.vishnoi3107@gmail.com>
2026-06-09 02:19:40 +0530

    fix build.sh path syntax

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
+
 # Install Flutter
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Verify flutter works
+flutter --version
 
 # Get dependencies and build
 flutter pub get
